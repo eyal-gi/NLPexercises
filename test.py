f9346e29aad6d7cd5ee0349dc18af97a6f885622
eyalgi <eyalg125@gmail.com>
2021-11-13 19:44:16 +0200

    initial commit of ex2

--- test.py
+++ test.py
@@ -11,16 +11,17 @@ text = 'A cat sat on the mat. A fat cat sat on the mat. A rat sat on the mat. Th
 # text = open('big.txt').read()
 # text = 'a cat sat a rat sat a bat sat'
 nt = ex1.normalize_text(text)
-print(nt)
+# print(nt)
 lm = ex1.Ngram_Language_Model(n=5, chars=True)
 lm.build_model(nt)  #*
-print(lm.get_model_dictionary())
+# print(lm.get_model_dictionary())
 
 # nt = re.sub('(?<! )(?=[.,!?()])|(?<=[.,!?()>])(?!abc)', r' ', "abc,")
 t = lm.generate('a ca', n=30)
-print(t)
+lm.evaluate(t)
+# print(t)
 # print(lm.evaluate(t))
-print(lm.evaluate("a cat sat on the mat . a fat cat sat on the mat . a bat spat on the mat . a rat sat on the mat ."))
+# print(lm.evaluate("a cat sat on the mat . a fat cat sat on the mat . a bat spat on the mat . a rat sat on the mat ."))
 # print(lm.evaluate("p a cat eyal ginosar gever on the mat"))
 # print(lm.evaluate(""))
 # print(lm.evaluate('the rat sat on the cat'))
