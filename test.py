34e59f817073438a7750abd6967d280af734741a
eyalgi <eyalg125@gmail.com>
2021-12-19 23:04:06 +0200

    LSTM docs

--- test.py
+++ test.py
@@ -16,6 +16,7 @@ lm.build_model(nt)
 sc = ex2.Spell_Checker()
 # sc.build_model(nt, 3)
 sc.add_language_model(lm)
+print(sc.evaluate("popup"))
 sc.add_error_tables(spelling_confusion_matrices.error_tables)
 print(sc.spell_check('you should forget aboutit', alpha=0.95))
 
